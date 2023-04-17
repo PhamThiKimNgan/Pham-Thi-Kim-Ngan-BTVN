94feaa86728c8fdf3e07dc6aaac3da28031aaebf
PhamThiKimNgan <130390372+PhamThiKimNgan@users.noreply.github.com>
2023-04-17 16:53:20 +0700

    Add files via upload

---
+++ bai4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class bai4 {
+public static void main(String[] args) {
+	Scanner sc = new Scanner (System.in);
+	int a,i;
+	System.out.println("nhập vào một số:");
+	a = sc.nextInt();
+	for (i = 1; i <= a; i++)
+	if (a==i)
+		System.out.println("Tháng " +i);
+}
+}
